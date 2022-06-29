06b549057a49b2ed8ade30b169b550894b002d75
divine <divineobi07@gmail.com>
2022-06-29 22:38:08 +0100

    add show map component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import ShowMap from "./components/ShowMap/ShowMap";
 
 const App = () => {
   return (
     <div className="main">
       <Header />
+      <ShowMap />
     </div>
   );
 };
---
+++ src/components/ShowMap/ShowMap.js
@@ -0,0 +1,12 @@
+import './ShowMap.css';
+
+const ShowMap = () => {
+    return (
+        <div className='show_map'>
+            lorem lorem
+        </div>
+    )
+}
+
+
+export default ShowMap;
